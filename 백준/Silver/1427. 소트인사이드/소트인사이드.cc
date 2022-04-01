85d3f3908c3b68e622f70a93e7c0dd46d4e627a5
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2022-04-01 20:15:14 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	char arr[11] = {};
+	cin >> arr;
+	int len = strlen(arr);
+	sort(arr, arr + len);
+	for (int i = len-1; i > -1; i--)
+	{
+		cout << arr[i];
+	}
+}
