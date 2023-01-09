5d95e20079d260204cadf48aa23fbc3422f221a0
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-01-09 17:56:00 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N, M, K, temp;
+    cin >> N >> M;
+    int **arr = new int *[N+1];
+    arr[0]=new int[M+1];
+    for(int i=0;i<=M;i++) arr[0][i]=0;
+    arr[1]=new int[M+1];
+    arr[1][0]=0;
+    cin >> arr[1][1];
+    for(int i=2;i<=M;i++) {
+        cin >> temp;
+        arr[1][i]=arr[1][i-1]+temp;
+    }
+    for(int i=2;i<=N;i++) {
+        arr[i] = new int[M+1];
+        arr[i][0]=0;
+        cin >> temp;
+        arr[i][1]=arr[i-1][1]+temp;
+        for(int j=2;j<=M;j++) {
+            cin >> temp;
+            arr[i][j]=arr[i-1][j]-arr[i-1][j-1]+arr[i][j-1]+temp;
+        }
+    }
+    cin >> K;
+    int x1, y1, x2, y2;
+    while (K--) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << arr[x2][y2]-arr[x1-1][y2]-arr[x2][y1-1]+arr[x1-1][y1-1] << "\n";
+        }
+}
