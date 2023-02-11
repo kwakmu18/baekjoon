9bb5c2ed3155aa2e0b7f1913a303d695467b183f
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-02-11 15:48:14 +0900

    [Gold III] Title: LCA, Time: 44 ms, Memory: 12340 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define LOG 21
 
 using namespace std;
 
 vector<vector<int>> tree;
+int N;
 int *depth;
-int *parent;
+int **parent;
 
 void dfs(int n, int d) {
     depth[n] = d;
@@ -13,34 +15,50 @@ void dfs(int n, int d) {
     for(int i=0;i<size;i++) {
         int newNode = tree[n][i];
         if (depth[newNode]!=0) continue;
-        parent[newNode] = n;
+        parent[newNode][0] = n;
         dfs(newNode, d+1);
     }
 }
 
+void set_parent() {
+    dfs(1,0);
+    parent[1][0]=1;
+    depth[1]=0;
+    for(int i=1;i<LOG;i++) {
+        for(int j=1;j<=N;j++) parent[j][i] = parent[parent[j][i-1]][i-1];
+    }
+}
+
 int lca(int a, int b) {
-    while (depth[a]!=depth[b]) {
-        if (depth[a]>depth[b]) a = parent[a];
-        else b = parent[b];
+    if (depth[a]>depth[b]) swap(a,b);
+    
+    for(int i=LOG-1;i>=0;i--) {
+        if (depth[b]-depth[a]>=(1<<i)) b=parent[b][i];
     }
 
-    while (a!=b) {
-        a = parent[a];
-        b = parent[b];
+    if (a==b) return a;
+
+    for(int i=LOG-1;i>=0;i--) {
+        if (parent[a][i]!=parent[b][i]) {
+            a=parent[a][i];
+            b=parent[b][i];
+        }
     }
-    
-    return a;
+    return parent[a][0];
 }
 
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    int N, M, A, B;
+    int M, A, B;
     cin >> N;
-
     tree.resize(N+1);
-    parent = new int[N+1]{0};
     depth = new int[N+1]{0};
+    parent = new int *[N+1]{0};
+    for(int i=0;i<=N;i++) {
+        parent[i] = new int[LOG];
+        for(int j=0;j<LOG;j++) parent[i][j]=0;
+    }
 
     for(int i=0;i<N-1;i++) {
         cin >> A >> B;
@@ -48,9 +66,7 @@ int main(void) {
         tree[B].push_back(A);
     }
 
-    dfs(1,0);
-    parent[1]=1;
-    depth[1]=0;
+    set_parent();
 
     cin >> M;
 
