b3fdddf9369f692a57c07095c5a87634d7bb3740
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-02-14 23:10:25 +0900

    [Bronze IV] Title: Acres, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    double N, M;
+    cin >> N >> M;
+
+    cout << ceil(N*M/5/4840);
+}
