b866949d3152f1447e12e9ad9f61be3d3128a8d2
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-02-14 20:06:59 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<bool> visited;
+vector<int> visitedNumbers;
+
+void dfs(int cnt) {
+    if (cnt==M) {
+        for(int i=0;i<M;i++) cout << visitedNumbers[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i=0;i<N;i++) {
+        if (visited[i] || (!visitedNumbers.empty() && visitedNumbers.back()>arr[i])) continue;
+        visitedNumbers.push_back(arr[i]);
+        visited[i]=true;
+        dfs(cnt+1);
+        visited[i]=false;
+        visitedNumbers.pop_back();
+    }
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+    arr.resize(N);
+    visited.resize(N);
+
+    for(int i=0;i<N;i++) {
+        cin >> arr[i];
+        visited[i]=false;
+    }
+
+    sort(arr.begin(), arr.end());
+
+    dfs(0);
+    
+    return 0;
+}
