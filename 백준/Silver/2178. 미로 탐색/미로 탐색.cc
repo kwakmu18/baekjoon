4941cd9b38f26a3ad619ae390504230dc4d2156a
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-02-18 03:50:53 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,47 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <string>
-
+#include<bits/stdc++.h>
+#define MAX 101 //100 -> doublefree 컴파일 에러
 using namespace std;
 
-int mov[4][2]={{-1,0}, {1,0}, {0,-1}, {0,1}};
-int main(void) {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	int h, w, nowh, noww, candh, candw;
-	string s;
+int a,b;
+bool arr[MAX][MAX];
+bool v[MAX][MAX];
 
-	cin >> h >> w;
+int bfs()
+{
+    queue<tuple<int,int,int>> q;
+    q.push({0,0,1});
+    v[0][0]=1;
+    while(!q.empty()){
+        int x=get<0>(q.front());
+        int y=get<1>(q.front());
+        int d=get<2>(q.front());
+        q.pop();
+        if(x==a-1 and y==b-1){
+            return d;
+        }
+        if(x-1>=0 and arr[x-1][y] and !v[x-1][y]){
+            q.push({x-1,y,d+1});
+            v[x-1][y]=1;
+        }
+        if(y-1>=0 and arr[x][y-1] and !v[x][y-1]){
+            q.push({x,y-1,d+1});
+            v[x][y-1]=1;
+        }
+        if(x+1<a and arr[x+1][y] and !v[x+1][y]){
+            q.push({x+1,y,d+1});
+            v[x+1][y]=1;
+        }
+        if(y+1<b and arr[x][y+1] and !v[x][y+1]){
+            q.push({x,y+1,d+1});
+            v[x][y+1]=1;
+        }
+    }
+}
 
-	vector<vector<bool>> maze(h);
-	vector<vector<int>> length(h);
-	for(int i=0;i<h;i++) {
-		maze[i].resize(w); length[i].resize(w);
-		cin >> s;
-		for(int j=0;j<w;j++) {
-			maze[i][j]=s[j]-'0';
-			length[i][j]=-1;
-		}
-	}
-	length[0][0]=1;
-	
-	queue<pair<char, char>> q; q.push({0,0});
-	while (!q.empty()) {
-		nowh=q.front().first; noww=q.front().second; q.pop();
-		for(int i=0;i<4;i++) {
-			candh=nowh+mov[i][0]; candw=noww+mov[i][1];
-			if (candh<0 || candh>=h || candw<0 || candw>=w || maze[candh][candw]==0) continue;
-			else if (length[candh][candw]==-1) {
-				length[candh][candw]=length[nowh][noww]+1;
-				q.push({candh, candw});
-			}
-			else if (length[candh][candw]<length[nowh][noww]+1) continue;
-			else {
-				length[candh][candw]=length[nowh][noww]+1;
-				//q.push({candh, candw});
-			}
-		}
-	}
-	cout << length[h-1][w-1];
-	return 0;
+int main()
+{
+    //cin.tie(NULL);
+    //cout.tie(NULL);
+    //ios_base::sync_with_stdio(false);
+
+    scanf("%d %d",&a,&b);
+
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            scanf("%1d",&arr[i][j]);
+        }
+    }
+
+    cout<<bfs();
 }
