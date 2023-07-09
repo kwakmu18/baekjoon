17e7b39c8c78da060bcfa6bfbe809e5fcd8004fd
kwakmu18 <95520640+kwakmu18@users.noreply.github.com>
2023-07-09 17:07:49 +0900

    [Gold V] Title: 최소비용 구하기, Time: 48 ms, Memory: 3716 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -2,43 +2,39 @@
 
 using namespace std;
 
+int N, M, A, B, C, src, dst;
+vector<vector<pair<int,int>>> bus;
+vector<int> cost;
+
 int main(void) {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	int N,M,A,B,T,X,Y,nowtime,nowtown;
-	cin >> N;
-	cin >> M;
-	vector<vector<pair<int,int>>> road(N);
-	vector<vector<int>> road_candidate(N);
-	for(int i=0;i<N;i++) {
-		road_candidate[i].resize(N);
-		for(int j=0;j<N;j++) road_candidate[i][j]=100000;
-	}
-	for(int i=0;i<M;i++) {
-		cin >> A >> B >> T;
-		if (road_candidate[A-1][B-1]>T) road_candidate[A-1][B-1]=T;
-	}
-	for(int i=0;i<N;i++) {
-		for(int j=0;j<N;j++) {
-			if (road_candidate[i][j]!=100000) road[i].push_back({road_candidate[i][j],j});
-		}
-	}
-	cin >> X >> Y;
-	vector<int> length(N);
-	for(int i=0;i<N;i++) length[i]=200000000;
-	length[X-1]=0;
-	priority_queue<pair<int,int>> pq;
-	pq.push({0,X-1});
-	while (!pq.empty()) {
-		pair<int,int> now=pq.top();
-		pq.pop();
-		nowtime=now.first; nowtown=now.second;
-		int size=road[nowtown].size();
-		for(int i=0;i<size;i++) {
-			if (length[road[nowtown][i].second]>nowtime+road[nowtown][i].first) {
-				length[road[nowtown][i].second]=nowtime+road[nowtown][i].first;
-				pq.push({nowtime+road[nowtown][i].first,road[nowtown][i].second});
-			}
-		}
-	}
-	cout << length[Y-1];
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    cin >> M;
+    bus.resize(N);
+    cost.resize(N);
+    for(int i=0;i<N;i++) cost[i] = INT_MAX;
+    for(int i=0;i<M;i++) {
+        cin >> A >> B >> C;
+        bus[A-1].push_back({C, B-1});
+    }
+    for(int i=0;i<N;i++) {
+        sort(bus[i].begin(), bus[i].end());
+    }
+
+    cin >> src >> dst;
+    priority_queue<pair<int,int>> pq;
+    pq.push({0,src-1});
+    while (!pq.empty()) {
+        pair<int,int> now = pq.top(); pq.pop();
+        int nowCost = now.first, nowCity = now.second;
+        int size = bus[nowCity].size();
+        for(int i=0;i<size;i++) {
+            int newCity = bus[nowCity][i].second, newCost = nowCost + bus[nowCity][i].first;
+            if (newCost<cost[newCity]) {
+                pq.push({newCost, newCity});
+                cost[newCity] = newCost;
+            }
+        }
+    }
+    cout << cost[dst-1];
 }
